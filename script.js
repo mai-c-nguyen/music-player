f0596b860a58a5768f9c1eb0cda4fc78ca9a426a
Mai Nguyen <mai.nguyen.gl@gmail.com>
2021-06-29 23:35:04 +0200

    Update JS

---
+++ script.js
@@ -0,0 +1,65 @@
+const image = document.querySelector("img");
+const title = document.getElementById("title");
+const artist = document.getElementById("artist");
+const music = document.querySelector("audio");
+const prevBtn = document.getElementById("prev");
+const playBtn = document.getElementById("play");
+const nextBtn = document.getElementById("next");
+
+// Music
+const songs = [
+  {
+    name: "jacinto-1",
+    displayName: "Electric Chill Machine",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "jacinto-2",
+    displayName: "Seven Nation Army (Remix",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "jacinto-3",
+    displayName: "Goodnight, Disco Queen",
+    artist: "Jacinto Design",
+  },
+  {
+    name: "metric-1",
+    displayName: "Front Row (Remix)",
+    artist: "Metric/Jacinto Design",
+  },
+];
+// Check if Playing
+let ifPlaying = false;
+
+// Play
+function playSong() {
+  ifPlaying = true;
+  playBtn.classList.replace("fa-play", "fa-pause");
+  playBtn.setAttribute("title", "Pause");
+  music.play();
+}
+// Pause
+function pauseSong() {
+  ifPlaying = false;
+  playBtn.classList.replace("fa-pause", "fa-play");
+  playBtn.setAttribute("title", "Play");
+  music.pause();
+}
+
+// Play or Pause Event Listener
+playBtn.addEventListener("click", () => (ifPlaying ? pauseSong() : playSong()));
+
+// Update DOM
+function loadSong(song) {
+  title.textContent = song.displayName;
+  artist.textContent = song.artist;
+  music.src = `music/${song.name}.mp3`;
+  image.src = `img/${song.name}.jpg`;
+}
+
+// On Load - Select First Song
+loadSong(songs[3]);
+
+// Event Listeners
+prevBtn.addEventListener("click", prevSong);
